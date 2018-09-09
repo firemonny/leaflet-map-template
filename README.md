94ed6a802254236cf6466874039a9442827314fd
MonnyChou <monny.chou@sensorup.com>
2018-09-09 11:37:30 -0600

    First commit: Project start

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 # leaflet-map-template
 
+The template is using Vue to generate a basic leaflet map
+
+## Getting Started
+
+The template provide a vue template for you starting using leaflet.
+The CSS is pre-installed in the template
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
 ```
---
+++ src/components/mapView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div id="map" class="map-settings"></div>
+    </div>    
+</template>
+<script>
+import L from "leaflet";
+export default {
+  data() {
+    return {
+      map: null,
+      tileLayer: null
+    };
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      this.map = L.map("map", {
+        center: [25.0419643, 121.5320798],
+        zoom: 10
+      });
+      this.tileLayer = L.tileLayer(
+        "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png",
+        {
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+          subdomains: "abcd",
+          maxZoom: 19
+        }
+      ).addTo(this.map);
+    }
+  }
+};
+</script>
+<style>
+.map-settings {
+  height: 600px;
+  width: 100%;
+}
+</style>
